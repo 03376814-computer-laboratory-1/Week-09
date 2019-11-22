4829dde4f7c1645fc92af39c6d0491a43f34d8ef
phruksa4165@outlook.com <phruksa4165@outlook.com>
2019-11-22 13:34:39 +0700

    commit2

--- 61030035/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030035/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -24,6 +24,13 @@ namespace GDIPlus_1
             Pen bluepen = new Pen(Color.Blue, 2);
             g.DrawRectangle(bluepen, 10, 10, 100, 100);
             bluepen.Dispose();
+
+            Pen somepen = new Pen(Color.FromArgb(255, 120, 200));
+            g.DrawEllipse(somepen, 20, 20, 200, 200);
+            somepen.Dispose();
+
+           
+                
         }
     }
 }
