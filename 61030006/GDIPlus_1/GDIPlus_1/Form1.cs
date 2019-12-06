5b77627d1a0c778ffd14d943a6eeaf66ffeca265
kkkanitar <khanita03272@gmail.com>
2019-12-06 21:49:31 +0700

    hgfds

--- 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -21,18 +21,21 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-
-            Pen penEllipse = new Pen(Color.Brown);
-            penEllipse.DashStyle = DashStyle.Dash;
-            g.DrawEllipse(penEllipse, 20, 20, 200, 150);
-
-
-            Pen penPie = new Pen(Color.Magenta, 2);
-            g.DrawPie(penPie, 20, 20, 200, 150, 45, 90);
-            Pen penPie1 = new Pen(Color.BlueViolet, 2);
-            g.DrawPie(penPie1, 20, 20, 200, 150, 150, 45);
-
+            GraphicsPath gpath = new GraphicsPath();
+            gpath.AddEllipse(46, 4, 28, 28);
+            gpath.AddLine(36, 32, 84, 32);
+            gpath.AddLine(100, 80, 88, 84);
+            gpath.AddLine(76, 50, 74, 84);
+            gpath.AddLine(90, 150, 74, 150);
+            gpath.AddLine(60, 100, 46, 150);
+            gpath.AddLine(32, 150, 46, 84);
+            gpath.AddLine(44, 50, 32, 84);
+            gpath.AddLine(20, 80, 36, 32);
+            g.FillPath(Brushes.Blue, gpath);
             g.Dispose();
+
+
+
 
 
         }
