09140cbacb32b0e8239acdf5cca5ea11ec1a7a40
kkkanitar <khanita03272@gmail.com>
2019-12-07 02:17:29 +0700

    jyrt

--- 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -22,18 +22,44 @@ namespace GDIPlus_1
         {
 
             Graphics g = e.Graphics;
-            Bitmap bmp = new Bitmap("C:\\corgi.PNG");
-            this.SetClientSizeCore(bmp.Width, bmp.Height);
-            Rectangle destRect = new Rectangle(0, 0, bmp.Width, bmp.Height);
-            Brush mybrush = new SolidBrush(Color.Coral);
-            g.DrawImage(bmp, destRect);
-            g.DrawString("Hello bum!!!",
-            new Font("Verdana", 30, FontStyle.Bold),
-            mybrush,
-            50,
-            0);
+            Rectangle body = new Rectangle(20, 20,300, 250);
+            g.FillEllipse(Brushes.White, body);
+
+            Rectangle body1 = new Rectangle(80, 80, 300, 250);
+            g.FillEllipse(Brushes.White, body1);
 
 
+            Rectangle body2 = new Rectangle(200, 120, 120, 250);
+            g.FillEllipse(Brushes.White, body2);
+
+            Rectangle body3 = new Rectangle(80, 80, 220, 170);
+            g.FillEllipse(Brushes.Yellow, body3);
+
+            
+            Rectangle eye1 = new Rectangle(135, 130, 30, 5);
+            g.FillEllipse(Brushes.Black, eye1);
+
+            Rectangle eye2 = new Rectangle(205, 130, 30, 5);
+            g.FillEllipse(Brushes.Black, eye2);
+
+            Rectangle mouth = new Rectangle(170, 150, 50, 10);
+            g.FillEllipse(Brushes.White, mouth);
+
+            Brush mouth1 = new SolidBrush(Color.Black);
+            Pen mouth2 = new Pen(mouth1, 2);
+            g.DrawEllipse(mouth2, 170, 150, 50, 10);
+            mouth1.Dispose();
+            mouth2.Dispose();
+
+
+
+            Brush mybrush = new SolidBrush(Color.RosyBrown);
+
+            g.DrawString("Gudetama",
+                new Font("Verdana", 30, FontStyle.Bold),
+                mybrush,
+                250,
+                0);
             g.Dispose();
         }
       }
