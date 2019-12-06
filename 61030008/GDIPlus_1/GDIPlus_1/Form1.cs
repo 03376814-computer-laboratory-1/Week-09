83c6e893d38895b7b18df7632c3865ef40999574
xijanispeace <janistar.jp@gmail.com>
2019-12-06 22:48:34 +0700

    add

---
+++ 61030008/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace GDIPlus_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Graphics g = e.Graphics;
+            Pen mypen = new Pen(Color.Blue, 1);
+            mypen.DashStyle = DashStyle.Dash;
+            g.DrawRectangle(mypen, 10, 10, 200, 200);
+            mypen.Width = 4;
+            mypen.Color = Color.Pink;
+            g.DrawEllipse(mypen, 10, 10, 200, 200);
+            mypen.Dispose();
+        }
+    }
+}
---
+++ 61030008/Lab09/Lab09/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
