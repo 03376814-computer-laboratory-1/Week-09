3e1eaf42648c426c95a6a54ceecfa100816fccf0
kkkanitar <khanita03272@gmail.com>
2019-12-06 20:19:24 +0700

    jhgfds

--- 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -21,9 +21,9 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Brush mybrush = new SolidBrush(Color.DarkGreen);
+            HatchBrush mybrush = new HatchBrush(HatchStyle.DarkVertical, Color.White, Color.Violet); ;
             Pen mypen = new Pen(mybrush, 5);
-            mypen.DashStyle = DashStyle.Dash;
+            
             g.DrawRectangle(mypen, 10, 10, 200, 200);
             mypen.Dispose();
             mybrush.Dispose();
