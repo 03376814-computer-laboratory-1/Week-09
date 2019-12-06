0e9d87c7149d371f1a9ee72a98029ca2aff45535
kkkanitar <khanita03272@gmail.com>
2019-12-06 21:32:48 +0700

    bvcx

--- 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -22,28 +22,14 @@ namespace GDIPlus_1
         {
             Graphics g = e.Graphics;
 
-            //Draw  circle
-            Rectangle rect = new Rectangle(10, 10, 100, 100);
-            g.DrawEllipse(Pens.Purple, rect);
+            Pen penEllipse = new Pen(Color.Brown);
+            penEllipse.DashStyle = DashStyle.Dash;
+            g.DrawEllipse(penEllipse, 20, 20, 200, 150);
 
 
+            Pen penArc = new Pen(Color.Magenta, 2);
+            g.DrawArc(penArc, 20, 20, 200, 150, 90, 180);
 
-            //Fill Circle
-            Rectangle rect1 = new Rectangle(10, 120, 100, 100);
-            g.FillEllipse(Brushes.DeepPink, rect1);
-
-
-
-            //Draw ellispe
-            Rectangle rect2 = new Rectangle(150, 10, 120, 100);
-            g.DrawEllipse(Pens.DarkSlateBlue, rect2);
-
-
-            //Fill ellipse
-            Rectangle rect3 = new Rectangle(150, 120, 120, 100);
-            g.FillEllipse(Brushes.Firebrick, rect3);
-
-       
             g.Dispose();
 
 
