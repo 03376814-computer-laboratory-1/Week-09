9e3dce0a6c3c26c03a8619e6d85366e0fb82e33b
kkkanitar <khanita03272@gmail.com>
2019-12-06 22:03:43 +0700

    grfdew

--- 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -20,16 +20,22 @@ namespace GDIPlus_1
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            this.SetClientSizeCore(500, 600);
             Graphics g = e.Graphics;
-            Point[] pt =
+            HatchBrush brush;
+            int x = 20;
+            int y = 20;
+            foreach (HatchStyle brushStyle in Enum.GetValues(typeof(HatchBrush)))
             {
-                new Point(10,22),
-            new Point(188, 246),
-            new Point(250,192),
-            new Point(220,48),
-            };
-            g.FillClosedCurve(Brushes.Blue, pt);
-            g.DrawClosedCurve(Pens.Red, pt);
+                brush = new HatchBrush(brushStyle, Color.Navy, Color.Yellow);
+                g.FillRectangle(brush, x, y, 40, 20);
+                y += 30;
+                if ((y + 30) > this.ClientSize.Height)
+                {
+                    y = 20;
+                    x += 150;
+                }
+            }
             g.Dispose();
 
 
