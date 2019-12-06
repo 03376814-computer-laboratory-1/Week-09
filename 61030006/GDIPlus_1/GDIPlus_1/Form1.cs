663294e02ec63dd435d9a7904852ba449423b5a1
kkkanitar <khanita03272@gmail.com>
2019-12-06 22:36:25 +0700

    fghj

--- 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -20,22 +20,14 @@ namespace GDIPlus_1
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            this.SetClientSizeCore(500, 600);
+            
             Graphics g = e.Graphics;
-            HatchBrush brush;
-            int x = 20;
-            int y = 20;
-            foreach (HatchStyle brushStyle in Enum.GetValues(typeof(HatchBrush)))
-            {
-                brush = new HatchBrush(brushStyle, Color.Navy, Color.Yellow);
-                g.FillRectangle(brush, x, y, 40, 20);
-                y += 30;
-                if ((y + 30) > this.ClientSize.Height)
-                {
-                    y = 20;
-                    x += 150;
-                }
-            }
+            Image image = Image.FromFile("This PC:\\corgi.PNG");
+            TextureBrush brush
+                 = new TextureBrush(image);
+            Rectangle rect = new Rectangle(10, 10, 180, 150);
+            g.FillEllipse(brush, rect);
+
             g.Dispose();
 
 
