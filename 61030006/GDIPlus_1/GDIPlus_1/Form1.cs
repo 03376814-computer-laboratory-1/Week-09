59241311492f7dc993429cdad5964751873c1bdc
kkkanitar <khanita03272@gmail.com>
2019-12-06 20:52:05 +0700

    hgfd

--- 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -21,14 +21,43 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen pen = new Pen(Color.Green, 2);
-            pen.DashStyle = DashStyle.DashDot;
-            e.Graphics.DrawLine(pen, 10, 80, 220, 80);
-            pen.Dispose();
-            pen = new Pen(Brushes.DeepSkyBlue, 4);
-            e.Graphics.DrawLine(pen, 10, 120, 220, 120);
-            pen.Dispose();
+            Pen[] objpen = new Pen[11];
+            for (int i = 0; i < 11; i++)
+            {
+                objpen[i] = new Pen(Color.Blue, 9);
+            }
+
+            //set pen styles
+            objpen[0].EndCap = LineCap.AnchorMask;
+            objpen[1].EndCap = LineCap.ArrowAnchor;
+            objpen[2].EndCap = LineCap.Custom;
+            objpen[3].EndCap = LineCap.DiamondAnchor;
+            objpen[4].EndCap = LineCap.Flat;
+            objpen[5].EndCap = LineCap.NoAnchor;
+            objpen[6].EndCap = LineCap.Round;
+            objpen[7].EndCap = LineCap.RoundAnchor;
+            objpen[8].EndCap = LineCap.Square;
+            objpen[9].EndCap = LineCap.SquareAnchor;
+            objpen[10].EndCap = LineCap.Triangle;
+
+            for (int i = 0; i < 11; i++)
+            {
+                g.DrawLine(objpen[i], 10, 10 + 20 * i, 200, 10 + 20 * i);
+                objpen[i].Dispose();
+
+            }
+            g.Dispose();
+
+
+
+
+
+
+
+
+
 
         }
+
     }
 }
