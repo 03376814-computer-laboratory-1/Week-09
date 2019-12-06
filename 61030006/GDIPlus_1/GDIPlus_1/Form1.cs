1c1fb9f1d964070e0f34c26d4c8124d0289e7f75
kkkanitar <khanita03272@gmail.com>
2019-12-06 20:32:32 +0700

    gfds

--- 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -21,12 +21,12 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            HatchBrush mybrush = new HatchBrush(HatchStyle.DarkVertical, Color.White, Color.Violet); ;
-            Pen mypen = new Pen(mybrush, 5);
-            
-            g.DrawRectangle(mypen, 10, 10, 200, 200);
-            mypen.Dispose();
-            mybrush.Dispose();
+            Pen blackpen = new Pen(Color.Black, 3);
+            Point ponit1 = new Point(100, 100);
+            Point ponit2 = new Point(200, 200);
+            e.Graphics.DrawLine(blackpen, ponit1, ponit2);
+            blackpen.Dispose();
+           
 
         }
     }
