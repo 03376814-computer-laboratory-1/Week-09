47513eabe85e843182315b886858b42d1a8a1ebc
kkkanitar <khanita03272@gmail.com>
2019-12-07 12:57:15 +0700

    gte

--- 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030006/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -41,6 +41,18 @@ namespace GDIPlus_1
                 Color.Yellow, Color.Navy, 90f, true);
             e.Graphics.FillRectangle(pnlGdt, panel2.ClientRectangle);
             pnlGdt.Dispose();
+
+        }
+
+        private void Panel3_Paint(object sender, PaintEventArgs e)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(panel3.ClientRectangle);
+            PathGradientBrush br = new PathGradientBrush(path);
+            br.CenterPoint = new PointF(panel3.ClientRectangle.Width / 2, panel3.ClientRectangle.Height / 2);
+            br.CenterColor = Color.Navy;
+            br.SurroundColors = new Color[] { Color.Yellow };
+            e.Graphics.FillPath(br, path);
         }
     }
     }
